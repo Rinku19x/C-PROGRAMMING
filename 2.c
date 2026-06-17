1f22ee74758827fb1937ed8ddb1f77c22076c6be
Rinku19x <rinkurathod1911@gmail.com>
2026-06-17 22:11:25 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,14 @@
+//WAP to convert the currency dollar into rupees//
+
+#include <stdio.h>
+    int main()
+    {
+        float dollar ,rs;
+            printf("Enter the dollar=");
+            scanf("%f",&dollar);
+
+        rs=dollar*45;
+            printf("Rupees=%f",rs);
+
+        return 0;
+    }
